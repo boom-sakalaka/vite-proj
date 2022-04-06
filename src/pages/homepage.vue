4652ddd85eaa26c44cf37a1d00caadfeb6b74314
guzhihao <936285702@qq.com>
2022-04-06 10:04:05 +0800

    feature: 添加 styleLint 的校验

--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -1,11 +1,11 @@
 <!--
  * @Date: 2022-03-31 14:04:56
- * @LastEditTime: 2022-04-02 09:38:21
+ * @LastEditTime: 2022-04-06 10:01:33
  * @FilePath: \vite-admin-project\src\pages\homepage.vue
  * @Description: 
 -->
 <template>
-    <div class="w-40 h-20 bg-slate-700 text-amber-900">
+    <div class="w-40 h-20 bg-slate-700 text-amber-900 box-container">
         首页
         <el-button>按钮</el-button>
     </div>
@@ -14,4 +14,13 @@
 <script setup lang="ts">
 import { ElButton } from 'element-plus';
 </script>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.box-container {
+    position: fixed;
+    top: 1px;
+    left: 1px;
+    width: 100px;
+    height: 100px;
+}
+</style>
